5816a84e206b2ca1d7af64344599f89e7e66bb6e
Viktor Zhurbin <vzhurbin@gmail.com>
2018-06-23 16:22:04 +0300

    add flow

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
+// @flow
 import React from 'react';
 
-// @flow
-class DateRange extends React.Component {
-  constructor(props) {
-    super(props)
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open('GET', 'https://yandex.com/time/sync.json?geo=213');
-    xmlHttp.send();
-    console.log(JSON.parse(xmlHttp.responseText));
+type Props = {};
+type State = {
+  date: ?number,
+  updateTime: ?Date,
+  isFocused: boolean,
+}
 
-    this.state = {
-      date: props.date,
-      updateTime: new Date(JSON.parse(xmlHttp.responseText).time)
-    }
-  }
-
+class DateRange extends React.Component<Props, State> {
   componentWillMount() {
-    this.refs.input.onFocus = () => this.refs.input.style.backgroundColor = '#900';
-    this.refs.input.onBlur = () => this.refs.input.style.backgroundColor = '#fff';
+    const date = this.getTime();
+    this.setState({
+      date: Date.now(),
+      updateTime: date,
+    })
   }
 
-  onChange = (value) => {
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open('GET', 'https://yandex.com/time/sync.json?geo=213', false);
-    xmlHttp.send();
+  getTime = () => {
+    // let xhr = new XMLHttpRequest();
+    // xhr.open("GET", "https://yandex.com/time/sync.json?geo=213");
+    // xhr.send();
+    // return new Date(JSON.parse(xmlHttp.responseText).time)
+    return new Date(Date.now());
+  }
 
-    if (this.state.date !== value) {
-      this.setState({ date: value })
+  onChange = (value: string) => {
+    const newDate = new Date(value).getTime();
+    if (this.state.date !== newDate) {
+      this.setState({ date: newDate })
     }
 
+    const date = this.getTime();
     this.setState({
-      updateTime: new Date(JSON.parse(xmlHttp.responseText).time)
+      updateTime: date,
     })
-
-    this.props.onChange(this.state)
   }
 
-  renderItems = (items) => {
+  renderItems = (items: number[]) => {
     return <div>{items.map((value, index) => <div key={index}>{value}</div>)}</div>
   }
 
-  createItems = (period) => {
-    let dates = [];
+  createItems = (period: { start: ?number, end: ?number }) => {
+    let i, dates = [];
     for (i = +period.start; i < +period.end; i += 3600000 * 168) {
       dates.push(i)
     }
@@ -49,63 +50,74 @@ class DateRange extends React.Component {
     let periods = [];
     for (i = 0; i < dates.length; i++) {
       let date = new Date(dates[i]);
-      if (date.getDay() == 1) period = `${date.toLocaleDateString()} - {date.setHours(168).toLocaleDateString()}`
-      else if (date.getDay() == 2) period[i] = `${date.setHours(-24).toLocaleDateString()} - {date.setHours(144).toLocaleDateString()}`
-      else if (date.getDay() == 3) period[i] = `${date.setHours(-48).toLocaleDateString()} - {date.setHours(120).toLocaleDateString()}`
-      else if (date.getDay() == 4) period[i] = `${date.setHours(-48).toLocaleDateString()} - {date.setHours(120).toLocaleDateString()}`
-      else if (date.getDay() == 5) period[i] = `${date.setHours(-72).toLocaleDateString()} - {date.setHours(96).toLocaleDateString()}`
-      else if (date.getDay() == 6) period[i] = `${date.setHours(-96).toLocaleDateString()} - {date.setHours(72).toLocaleDateString()}`
-      else if (date.getDay() == 0) period[i] = `${date.setHours(-120).toLocaleDateString()} - {date.setHours(48).toLocaleDateString()}`
+      if (date.getDay() == 1) period = `${date} - ${date.setHours(168)}`
+      else if (date.getDay() == 2) period[i] = `${date.setHours(-24)} - ${date.setHours(144)}`
+      else if (date.getDay() == 3) period[i] = `${date.setHours(-48)} - ${date.setHours(120)}`
+      else if (date.getDay() == 4) period[i] = `${date.setHours(-48)} - ${date.setHours(120)}`
+      else if (date.getDay() == 5) period[i] = `${date.setHours(-72)} - ${date.setHours(96)}`
+      else if (date.getDay() == 6) period[i] = `${date.setHours(-96)} - ${date.setHours(72)}`
+      else if (date.getDay() == 0) period[i] = `${date.setHours(-120)} - ${date.setHours(48)}`
 
-      var n = periods.length, a = periods.length, b;
-      do {
-        b = false;
-        a /= 1.3;
-        if (a == 9 || a == 10) a = 11;
-        if (a < 1) a = 1;
-        for (var i = 0; i < n - a; ++i) {
-          if (periods[i] > periods[i + a]) {
-            b = true;
-            var t = periods[i + a]; periods[i + a] = periods[i]; periods[i] = t;
-          }
-        }
-      } while (a > 1 || b);
+      // let n = periods.length, a = periods.length, b;
+      // do {
+      //   b = false;
+      //   a /= 1.3;
+      //   if (a == 9 || a == 10) a = 11;
+      //   if (a < 1) a = 1;
+      //   for (i = 0; i < n - a; ++i) {
+      //     if (periods[i] > periods[i + a]) {
+      //       b = true;
+      //       let t = periods[i + a]; periods[i + a] = periods[i]; periods[i] = t;
+      //     }
+      //   }
+      // } while (a > 1 || b);
     }
+
     return periods;
   }
 
-  createPeriod = (date) => {
-    let newDate = date;
-    newDate.year = newDate.year + 1;
-    return {
-      start: date,
-      end: newDate
-    }
+  toggleFocus = (state: boolean) => {
+    this.setState({ isFocused: !state })
   }
 
-  renderView = (props) => {
-    return ( // разбитый на строки возврат надо обернуть в скобки
+  createPeriod = (date: ?number) => {
+    let newDate = new Date(date);
+    newDate.setFullYear(newDate.getFullYear() + 1);
+    return {
+      start: Date,
+      end: Date,
+    } = {
+        start: date,
+        end: newDate.getTime(),
+      }
+  }
+
+  render() {
+    const { date, updateTime, isFocused } = this.state;
+    const day = updateTime.getDate();
+    const rawMonth = updateTime.getMonth() + 1;
+    const month = rawMonth > 9 ? rawMonth : `0${rawMonth}`;
+    const period = this.createPeriod(date);
+    const color = isFocused ? '#f00' : '#fff';
+
+    return (
       <div>
         <div>
-          <input type="date" ref="input" onChange={function (event) { this.onChange(event.target.value) }} />
+          <input
+            type="date"
+            style={{ backgroundColor: color }}
+            onChange={(event) => { this.onChange(event.target.value) }}
+            onFocus={() => this.toggleFocus(isFocused)}
+            onBlur={() => this.toggleFocus(isFocused)}
+          />
         </div>
         <div>
-          {`Последнее изменение: ${props.updateTime.getDate() + '.' + props.updateTime.getMonth() > 9
-            ? '0' + props.updateTime.getMonth()
-            : props.updateTime.getMonth()}`}
+          {`Последнее изменение: ${day}.${month}`}
         </div>
         <div>
-          {this.renderItems(this.createItems(props.period))}
+          {this.renderItems(this.createItems(period))}
         </div>
       </div>)
-  }
-
-  render() { // отсутствовала фигурная скобка
-    return React.CreateElement(this.renderView, {
-      period: this.createPeriod(this.props.date),
-      updateTime: this.props.updateTime,
-      onChange: this.onChange,
-    })
   }
 }
 
